5407816cd1f9aaf8b12fd842d9634e8b4afefb66
Estevam Melo Barbosa <Melo Barbosa@MeloBarbosa-PC.(none)>
2016-11-22 19:40:29 -0200

    exerccioaula4 calculadora #2

---
+++ Exercise/Exerciseaula4/Assets/js/main.js
@@ -0,0 +1,25 @@
+
+function somar(){
+	 var n1 = document.getElementById('frs').value;
+	 var n2 = document.getElementById('scn').value;
+	 var n3 = parseInt(n1) + parseInt(n2);
+	 	alert(n3);
+}
+function subtrair(){
+	 var n1 = document.getElementById('frs').value;
+	 var n2 = document.getElementById('scn').value;
+	 var n3 = parseInt(n1) - parseInt(n2);
+	 	alert(n3);
+}
+function dividir(){
+	 var n1 = document.getElementById('frs').value;
+	 var n2 = document.getElementById('scn').value;
+	 var n3 = parseInt(n1) / parseInt(n2);
+	 	alert(n3);
+}
+function multiplicar(){
+	 var n1 = document.getElementById('frs').value;
+	 var n2 = document.getElementById('scn').value;
+	 var n3 = parseInt(n1) * parseInt(n2);
+	 	alert(n3);
+}
--- Exercise/Exerciseaula4/exemplo.js
+++
@@ -1,5 +0,0 @@
-
-var v1 = 3;
-var v2 = 7;
-var v3 = 'total: ';
-alert(v3 + (v1 + v2));
